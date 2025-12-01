ec3bfdeaad85ad67f097ce8948a45b83ee6edb30
luis <luisbernalgutierrez66@gmail.com>
2025-12-01 01:52:25 -0600

    Eliminar archivo .env.example y agregar seeder para crear datos de prueba en los modelos Cliente, Producto, ProductoVariante, Servicio, EstadoPedido, Pedido, PedidoItem, Entrega y PedidoEstadosHistorial.

---
+++ database/migrations/2025_11_30_021448_create_entregas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entregas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained()->onDelete('cascade');
+            $table->dateTime('fecha_programada')->nullable();
+            $table->dateTime('fecha_entregada')->nullable();
+            $table->string('recibio_cliente')->nullable();
+            $table->string('ubicacion')->nullable();
+            $table->text('notas')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entregas');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,15 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Cliente;
+use App\Models\Producto;
+use App\Models\ProductoVariante;
+use App\Models\Servicio;
+use App\Models\Pedido;
+use App\Models\PedidoItem;
+use App\Models\EstadoPedido;
+use App\Models\Entrega;
+use App\Models\PedidoEstadosHistorial;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +30,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+         Cliente::factory()->count(10)->create();
+        Producto::factory()->count(20)->create();
+        ProductoVariante::factory()->count(30)->create();
+        Servicio::factory()->count(10)->create();
+        EstadoPedido::factory()->count(4)->create();
+        Pedido::factory()->count(15)->create();
+        PedidoItem::factory()->count(40)->create();
+        Entrega::factory()->count(10)->create();
+        PedidoEstadosHistorial::factory()->count(20)->create();
     }
 }
